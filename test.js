0bc319e5b15816a573bc5b624fa887e18f88f728
Igor Berbasov <iberbasov@bk.ru>
2022-10-04 09:03:27 +0400

    1

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
-const button = document.querySelector('#button');
-button.addEventListener('click', clearParags);
+const input = document.getElementById('inp');
+const button = document.getElementById('btn');
+const hint = document.querySelector('.hint');
 
-function clearParags() {
-    let parags = document.querySelectorAll('.parag');
-    parags.forEach(element => {
-        element.textContent = "Cleared text";
-    });
-}
+input.addEventListener('input', function() {
+    console.log(this.value.length);
+    this.classList = '';
+    if(this.value.length <= 5) {
+        hint.textContent = (5 - this.value.length) + " chars left";
+        this.classList.add('right');
+    } else {
+        hint.textContent = (this.value.length - 5) + " chars exceeded";
+        this.classList.add('wrong');
+    }
+})
+
+button.addEventListener('click', () => {
+    input.focus();
+})
