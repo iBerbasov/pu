fe0598b0f31a237f31063fc2efe3bcad1da0df88
Igor Berbasov <iberbasov@bk.ru>
2022-10-05 16:10:22 +0400

    adding new rows to list

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
-let elem = document.querySelector('button');
+const div = document.querySelector('div');
+const p = document.querySelector('p');
 
-elem.addEventListener('click', function(event) {
-	console.log(event.code);
-    if(event.altKey) {
-        console.log('alt down');
+div.addEventListener('click', function(event) {
+    let elem = event.target;
+    if(elem.tagName === 'LI'){
+        elem.textContent += "!";
+    } else if (elem.tagName === 'UL') {
+        let newRow = elem.children[0].cloneNode(true);
+        newRow.textContent = elem.children.length + 1;
+        elem.append(newRow);
+    } else {
+        console.log('div click');
     }
-    if(event.shiftKey) {
-        console.log('shift down');
-    }
-    if(event.ctrlKey) {
-        console.log('ctrl down');
-    }
-});
+})
